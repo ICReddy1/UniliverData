2171728ab360d1aaad3963bead1448922ea4c6bb
china-indukuri <sujithadevig@gmail.com>
2021-04-26 16:22:43 +0530

    refactor

---
+++ com/uniliver/source_data_loading.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, IntegerType, BooleanType,DoubleType
+import os.path
+import yaml
+
+if __name__ == '__main__':
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read Files") \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
+        .getOrCreate()
+        # .master('local[*]') \
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
+                   "lowerBound": "1",
+                   "upperBound": "100",
+                   "dbtable": app_conf["mysql_conf"]["dbtable"],
+                   "numPartitions": "2",
+                   "partitionColumn": app_conf["mysql_conf"]["partition_column"],
+                   "user": app_secret["mysql_conf"]["username"],
+                   "password": app_secret["mysql_conf"]["password"]
+                   }
+    # print(jdbcParams)
+
+    # use the ** operator/un-packer to treat a python dictionary as **kwargs
+    print("\nReading data from MySQL DB using SparkSession.read.format(),")
+    txnDF = spark \
+        .read.format("jdbc") \
+        .option("driver", "com.mysql.cj.jdbc.Driver") \
+        .options(**jdbc_params) \
+        .load()
+
+    txnDF.show()
+
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" test/source_data_loading.py
