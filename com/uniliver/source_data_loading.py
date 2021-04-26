0a6f721bf82d3de24f19c99a3f8df446ebc46ae9
china-indukuri <sujithadevig@gmail.com>
2021-04-26 23:58:35 +0530

    refactor

--- com/uniliver/source_data_loading.py
+++ com/uniliver/source_data_loading.py
@@ -70,7 +70,8 @@ if __name__ == '__main__':
                 .option("pem", os.path.abspath(current_dir + "/../../" + app_secret["sftp_conf"]["pem"])) \
                 .option("fileType", "csv") \
                 .option("delimiter", ",") \
-                .load(src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
+                .load(src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")\
+                ..withColumn("ins_dt",functions.current_date())
 
             txn_df2.show(5, False)
 
