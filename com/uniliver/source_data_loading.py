8fa87f18e5bcbd355b6549971405af81b6fb0d9e
china-indukuri <sujithadevig@gmail.com>
2021-04-26 17:56:24 +0530

    refactor

--- com/uniliver/source_data_loading.py
+++ com/uniliver/source_data_loading.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
     spark.sparkContext.setLogLevel('ERROR')
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
@@ -27,22 +27,22 @@ if __name__ == '__main__':
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
     jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
-                   "lowerBound": "1",
-                   "upperBound": "100",
-                   "dbtable": app_conf["mysql_conf"]["dbtable"],
-                   "numPartitions": "2",
-                   "partitionColumn": app_conf["mysql_conf"]["partition_column"],
-                   "user": app_secret["mysql_conf"]["username"],
-                   "password": app_secret["mysql_conf"]["password"]
+                  "lowerBound": "1",
+                  "upperBound": "100",
+                  "dbtable": app_conf["mysql_conf"]["dbtable"],
+                  "numPartitions": "2",
+                  "partitionColumn": app_conf["mysql_conf"]["partition_column"],
+                  "user": app_secret["mysql_conf"]["username"],
+                  "password": app_secret["mysql_conf"]["password"]
                    }
     # print(jdbcParams)
 
     # use the ** operator/un-packer to treat a python dictionary as **kwargs
     print("\nReading data from MySQL DB using SparkSession.read.format(),")
-    txnDF = spark \
-        .read.format("jdbc") \
-        .option("driver", "com.mysql.cj.jdbc.Driver") \
-        .options(**jdbc_params) \
+    txnDF = spark\
+        .read.format("jdbc")\
+        .option("driver", "com.mysql.cj.jdbc.Driver")\
+        .options(**jdbc_params)\
         .load()
 
     txnDF.show()
