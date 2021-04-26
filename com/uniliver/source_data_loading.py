2dee901b9c0abe57c392c653bb17f0916772dbfb
china-indukuri <sujithadevig@gmail.com>
2021-04-26 16:38:30 +0530

    refactor

--- com/uniliver/source_data_loading.py
+++ com/uniliver/source_data_loading.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     spark.sparkContext.setLogLevel('ERROR')
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../" + ".secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
@@ -43,4 +43,4 @@ if __name__ == '__main__':
 
     txnDF.show()
 
-# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" test/source_data_loading.py
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" com/uniliver/source_data_loading.py
